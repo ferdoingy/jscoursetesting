34492dac80507508c3c2f48eda87742a00d23caf
ferdoingy <ferdoingy@gmail.com>
2026-08-26 19:53:40 -0400

    Ig followers counter

---
+++ followersCount/followers_count.js
@@ -0,0 +1,25 @@
+let count = 0; // Initialize count to 0
+
+function increaseCount() {
+    count++;
+    displayCount();
+    checkCountValue();
+}
+
+function displayCount() {
+    document.getElementById("countDisplay").innerHTML = count; 
+}
+
+function checkCountValue() {
+    if (count === 10)
+    {
+        alert("Your ig post gained 10 followers!");
+    }
+    else
+    {
+        if (count === 20)
+        {
+            alert("Your ig post gained 20 followers!");
+        }
+    }
+}
