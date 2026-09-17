d624b764026ff53681a9ef65f93858142663fe5a
ferdoingy <ferdoingy@gmail.com>
2026-09-16 23:32:36 -0500

    Coordinates added for OpenWeather API

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -3,7 +3,7 @@ function showweatherDetails(event)
     event.preventDefault();
     const city = document.getElementById('city').value;
     const apiKey = 'ab9728277d604becdad715a7f5e4204d';
-    const apiURL = 'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}';
+    const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
     fetch(apiURL).then(response => response.json()).then(data => 
     {
@@ -17,3 +17,26 @@ function showweatherDetails(event)
 }
 
 document.getElementById('weatherForm').addEventListener('submit', showweatherDetails);
+
+function showWeatherCoordinates(event)
+{
+    event.preventDefault();
+    const longitude = document.getElementById('longitude').value;
+    const latitude = document.getElementById('latitude').value;
+    const apiKey = 'ab9728277d604becdad715a7f5e4204d';
+    const apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    fetch(apiURL)
+        .then(response => response.json())
+        .then(data =>
+        {
+            console.log(data);
+            const weatherInfoLL = document.getElementById('weatherInfoLL');
+            weatherInfoLL.innerHTML = `<h2>Weather in ${data.name}</h2>
+                                        <p>Temperature: ${data.main.temp}</p>
+                                        <p>Weather: ${data.weather[0].description}</p>`;
+        }
+        )
+}
+
+document.getElementById('weatherCoordinates').addEventListener('submit', showWeatherCoordinates);
